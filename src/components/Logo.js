98ab875acaf5526b4f0017bea77c049038db4957
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-03-28 11:32:00 +0100

    changed Logo.js to function export

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,8 +1,12 @@
-import React, { Component } from "react";
+import React from "react";
 import logo from "../logo.svg";
 
-export default class Logo extends Component {
-  render() {
-    return <img src={logo} className="App-logo" alt="logo" />;
-  }
-}
+/* function */
+const Logo = () => <img src={logo} className="App-logo" alt="logo" />;
+export default Logo;
+/* alternative class
+ export default class Logo extends Component {
+   render() {
+     return <img src={logo} className="App-logo" alt="logo" />;
+   }
+ }*/
